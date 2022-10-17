5920a1e9781a4528d277fc949663c3349be56ff8
Crypt0zauruS <101745417+Crypt0zauruS@users.noreply.github.com>
2022-10-17 15:10:31 +0400

    Inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+  const [author, setAuthor] = useState("");
+  const [color, setColor] = useState("");
+  const colors = [
+    "#16a085",
+    "#27ae60",
+    "#f39c12",
+    "#e74c3c",
+    "#9b59b6",
+    "#FB6964",
+    "#BDBB99",
+    "#77B1A9",
+    "#73A857",
+  ];
+
+  const reactQuoteMachineApi = function () {
+    axios
+      .get("https://api.quotable.io/random")
+      .then((response) => {
+        setText(response.data.content);
+        setAuthor(response.data.author);
+        setColor(colors[Math.floor(Math.random() * colors.length)]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    reactQuoteMachineApi();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        <div id="quote-box" style={{ backgroundColor: `${color}` }}>
+          <div id="text">
+            <p>{text}</p>
+          </div>
+          <div id="author">
+            <h3>{author}</h3>
+          </div>
+          <button id="new-quote" onClick={reactQuoteMachineApi}>
+            New Quote
+          </button>
+          <br />
+          <a
+            id="tweet-quote"
+            href={`https://twitter.com/intent/tweet?text=${text}`}
+            target="_blank"
+          >
+            Tweet the quote !
+          </a>
+        </div>
+      </>
     </div>
   );
 }
