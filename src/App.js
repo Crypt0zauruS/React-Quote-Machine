a33615b405091fefba8e7dda83ca2c26f722646a
Crypt0zauruS <101745417+Crypt0zauruS@users.noreply.github.com>
2022-10-18 15:04:18 +0400

    improved CSS,added licensing infos and profiles link

--- src/App.js
+++ src/App.js
@@ -59,6 +59,30 @@ function App() {
           </a>
         </div>
       </>
+      <div className="footer">
+        &copy; Copyright by Crypt0zauruS
+        <p>
+          <br />
+          Follow me on
+          <a
+            className="twitter"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://twitter.com/CryptosaurusRe4"
+          >
+            <i className="fab fa-twitter"></i>
+          </a>
+          and
+          <a
+            className="github"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/Crypt0zauruS"
+          >
+            <i className="fab fa-github"></i>
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
